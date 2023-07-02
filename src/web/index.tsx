b13b5710e9b8c5eea838ca8029a5184caaf901cb
liushuhao <liushuhao@sina.com>
2023-07-02 21:41:30 +0800

    2

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,37 +1,36 @@
 /*
- * @Description: 
+ * @Description:
  * @Version: 2.0
  * @Autor: liushuhao
  * @Date: 2022-07-01 09:14:34
  * @LastEditors: liushuhao
- * @LastEditTime: 2023-07-02 13:40:21
+ * @LastEditTime: 2023-07-02 21:41:04
  */
-import './wdyr'
-import './public-path'
+import './wdyr';
+import './public-path';
 import App from '@pages/App';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, HashRouter  } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './style.css';
 import 'antd/dist/antd.less';
 import zhCN from 'antd/es/locale/zh_CN';
 import { emitter } from './mitt';
 import { initCommunicationProtocol } from './common';
 import { ConfigProvider } from 'antd';
-import '@assets/reset.css'
-import '@assets/css/index.less'
+import '@assets/reset.css';
+import '@assets/css/index.less';
 import '@assets/svg/iconfont';
 import '@assets/svg/iconfont.css';
-import useStore  from '@web/store/index'
-
+import useStore from '@web/store/index';
 
 let root: any;
 function render(props: any) {
   const { container } = props;
-  const { getState } = useStore
-  root = createRoot(container? container.querySelector('#app')! : document.querySelector('#app'));
+  const { getState } = useStore;
+  root = createRoot(container ? container.querySelector('#app')! : document.querySelector('#app'));
   root.render(
-    <BrowserRouter basename={window.__POWERED_BY_QIANKUN__ ? '/city' : '/'} >
-      <ConfigProvider locale={zhCN} prefixCls='jiuzhoumap'>
+    <BrowserRouter basename={window.__POWERED_BY_QIANKUN__ ? '/city' : '/'}>
+      <ConfigProvider locale={zhCN} prefixCls="jiuzhoumap">
         <App />
       </ConfigProvider>
     </BrowserRouter>
@@ -43,12 +42,12 @@ if (!window.__POWERED_BY_QIANKUN__) {
 }
 
 export async function bootstrap() {
-  console.log('[map] react app bootstraped');
+  console.log('[map]bootstraped');
 }
 
 export async function mount(props: any) {
   render(props);
-  initCommunicationProtocol(props)
+  initCommunicationProtocol(props);
   console.log('[map] props from main framework', props);
 }
 
