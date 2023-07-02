1cdb9bee854b1479435bacc5e714a4e9ba172e4d
liushuhao <liushuhao@sina.com>
2023-07-02 22:42:32 +0800

    tijiao

--- src/web/pages/App.tsx
+++ src/web/pages/App.tsx
@@ -4,11 +4,10 @@
  * @Autor: liushuhao
  * @Date: 2022-06-28 23:23:42
  * @LastEditors: liushuhao
- * @LastEditTime: 2023-06-09 17:29:53
+ * @LastEditTime: 2023-07-02 22:24:22
  */
 import './App.css';
 import LayoutMain from '@layouts/layoutsMain'
-
 
 const App = (): JSX.Element => {
   return (
